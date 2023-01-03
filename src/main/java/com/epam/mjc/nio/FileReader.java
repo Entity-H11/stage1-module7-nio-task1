49c6bc7f35b1713f310423e9f67ab1676d36af7b
emmanuel hernandez <emmanuelhern00@gmail.com>
2023-01-02 23:42:32 -0400

    reading a file

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 
 public class FileReader {
+    Logger logger = Logger.getLogger(FileReader.class.getName());
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> properties = new HashMap<>();
+        try (BufferedReader input = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while (Objects.nonNull(line = input.readLine())) {
+                String[] splited = line.split(": ");
+                properties.put(splited[0], splited[1]);
+            }
+        } catch (FileNotFoundException e) {
+            logger.log(Level.SEVERE, e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Profile(properties.get("Name"),
+                Integer.valueOf(properties.get("Age")),
+                properties.get("Email"),
+                Long.valueOf(properties.get("Phone")));
     }
 }
+
+
